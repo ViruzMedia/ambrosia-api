40b5c080479a4b9f9cb46d72df089ed194a5dc2d
ViruzMedia <mczarnecki636@gmail.com>
2022-12-28 03:40:12 +0100

    Update

--- misc/config.misc.js
+++ misc/config.misc.js
@@ -3,12 +3,12 @@
 
 module.exports = {
     //config for Database
-    db_string: "mongodb+srv://ambrosia:Z8GuZGhdiZ3SiyE@cluster0-det08.mongodb.net/dev_ambrosia?retryWrites=true",
+    db_string: "mongodb+srv://",
 
     //config for API
     api_domain: "http://localhost",
     api_port: "1888",
-    api_secret: "ahw56pzhon34oj#+ä3456losü+/(Z",
+    api_secret: "",
 
     //config for logger
     log_mode: "dev",
